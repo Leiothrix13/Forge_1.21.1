81b77d3fd87e72dfc19349b92f537973db8613ff
Leiothrix13 <ryanf11607@gmail.com>
2025-06-12 22:24:58 -0400

    Final product

--- src/main/java/net/Ryan/tutorialmod/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/net/Ryan/tutorialmod/datagen/ModGlobalLootModifierProvider.java
@@ -23,5 +23,42 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
                 new AddItemModifier(new LootItemCondition[]{
                         new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/buried_treasure")).build()
                 }, Moditems.CANOPY_MUSIC_DISC.get()));
+
+        this.add("cavern_crystal_from_mineshaft",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/abandoned_mineshaft")).build()
+                }, Moditems.CAVERN_CRYSTAL.get()));
+
+        this.add("desert_crystal_from_desert_temple",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/desert_pyramid")).build()
+                }, Moditems.DESERT_CRYSTAL.get()));
+
+        this.add("earthen_crystal_from_blacksmith",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/village/village_weaponsmith")).build()
+                }, Moditems.EARTHEN_CRYSTAL.get()));
+
+        this.add("forest_crystal_from_mansion",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/woodland_mansion")).build()
+                }, Moditems.FOREST_CRYSTAL.get()));
+
+        this.add("jungle_crystal_from_jungle_temple",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/jungle_temple")).build()
+                }, Moditems.JUNGLE_CRYSTAL.get()));
+
+        this.add("ocean_crystal_from_treasure",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/buried_treasure")).build()
+                }, Moditems.OCEAN_CRYSTAL.get()));
+
+        this.add("snow_crystal_from_igloo",
+                new AddItemModifier(new LootItemCondition[]{
+                        new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/igloo_chest")).build()
+                }, Moditems.SNOW_CRYSTAL.get()));
     }
+
+
 }
